8609076920a0f6f7d0ff4de790f808b45ac3492d
Ingole987 <pratik.ingole98@gmail.com>
2022-01-23 15:28:50 +0530

    added:Day13Assignments

---
+++ GenericMaximum.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumNumber
+{
+    
+    public class GenericMaximum<T> where T:IComparable//Icomparable gives constraints to T means it asks to read only values from IComparable
+    {
+        public T[] value;
+        public GenericMaximum(T[] value)//ctor and type tab tab to create a constructor(shortcut key)
+        {
+            this.value = value;// "this" indicates calling from global variable  
+           
+        }
+        public T[] Sort(T[] value)
+        {
+            Array.Sort(value);
+            return value;
+        }
+        public T MaxValue(params T[] value)
+        {
+            var sorted_values = Sort(value);//var sorted_values here sorted_values is only name can be changed
+            return sorted_values[sorted_values.Length-1];
+        }
+        public T MaxMethod()
+        {
+            var max = MaxValue(this.value);
+            return max;
+        }
+        public void PrintMaxValue()
+        {
+            var max= MaxValue(this.value);
+            Console.WriteLine(" Maximum value is: "+max);
+        }
+
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FindMaximumNumber;
+
+Console.WriteLine("Find max Number");
+int output = MaximumNumberCheck.MaximumIntegerNumber(11, 22, 33);
+Console.WriteLine(output);
+double doubleoutput = MaximumNumberCheck.MaximumFloatNumber(111.2, 22.45, 78.56);
+Console.WriteLine(doubleoutput);
+string stringoutput = MaximumNumberCheck.MaximumStringNumber("22", "33", "44");
+Console.WriteLine(stringoutput);
+
+int[] intArray = { 112, 344, 432, 555, 678 };
+GenericMaximum<int> generic = new GenericMaximum<int>(intArray);
+generic.PrintMaxValue();
+double[] doubleArray = { 11.2, 34.4, 4.32, 55.5, 6.78 };
+GenericMaximum<double> genericDouble = new GenericMaximum<double>(doubleArray);
+genericDouble.PrintMaxValue();
+string[] stringArray = { "111", "222", "333", "55", "999" };
+GenericMaximum<string> genericString = new GenericMaximum<string>(stringArray);
+//GenericMaximum<Employee> genericString = new GenericMaximum<string>(stringArray);
+genericString.PrintMaxValue();
+Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//public static int MaximumIntegerNumber(int firstValue, int SecondValue, int ThirdValue)
+//{
+//3,5,2 //7,3,6 //7,3,9
+//if (firstValue > SecondValue && firstValue > ThirdValue)
+//{
+//    Console.WriteLine("The Biggest Number Is:" + firstValue);
+//}
+//else if (SecondValue > ThirdValue)
+//{
+//    Console.WriteLine("The Biggest Number Is:" + SecondValue);
+//}
+//else
+//{
+//    Console.WriteLine("The Biggest Number Is:" + ThirdValue);
+//}
